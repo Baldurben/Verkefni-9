34fca29c3ddd96fa9cc729752c4a8a5bff06d7a6
baldurben <bab42@hi.is>
2019-11-13 19:08:13 +0000

    byrja

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
-const API_URL = 'https://apis.is/company?name=';
+const API_URL = 'https://apis.is/company?name=elko';
+
 
 /**
  * Leit að fyrirtækjum á Íslandi gegnum apis.is
  */
 const program = (() => {
   function init(companies) {
-
-  }
+	fetch(API_URL);
+	.then(result => {
+	if (!result.ok) {
+		throw new Error('Non 200 status');
+	}
+	return result.json();
+	})
+	.then(data => console.log(data));
+	.catch(error => console.error(error));
+	})
+	
+	}
 
   return {
     init,
