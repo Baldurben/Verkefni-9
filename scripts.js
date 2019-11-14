d4c57832b3228dfd3522cdc2213f860ee94a02be
baldurben <bab42@hi.is>
2019-11-14 11:26:01 +0000

    sækja json

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-const API_URL = 'https://apis.is/company?name=elko';
-
-
+const companies = document.getElementById('companies');
+let gif = document.createElement("img");
+let texti = document.createElement("P");
 /**
  * Leit að fyrirtækjum á Íslandi gegnum apis.is
  */
@@ -14,8 +14,34 @@ const program = (() => {
   }
 });
 
-document.getElementById('input').addEventListener('click', function(event) {
+function emptystring() {
+	var villa = document.createElement("P");
+	villa.innerHTML = "Fyrirtæki má ekki vera autt";
+	companies.appendChild(villa);
+}
+function loading() {
+	texti.innerHTML = "Loading...";
+	texti.setAttribute("class", "loading")
+	gif.setAttribute("src", "loading.gif");
+	gif.setAttribute("class", "loading")
+	texti.appendChild(gif);
+	companies.appendChild(texti);
+}
+
+
+document.getElementById('takki').addEventListener('click', function(event) {
 	event.preventDefault();
+	while (companies.firstChild) {
+	companies.removeChild(companies.firstChild);
+	}
+	var inputvalue = document.getElementById('input').value;
+	if(inputvalue.trim() == '') {
+		emptystring();
+	}
+	else {
+	let API_URL = 'https://apis.is/company?name=';
+	API_URL += inputvalue;
+	loading();
 	fetch(API_URL)
 	.then(result => {
 	if (!result.ok) {
@@ -23,10 +49,19 @@ document.getElementById('input').addEventListener('click', function(event) {
 	}
 	return result.json();
 	})
-	.then(data => console.log(data))
+	.then(data => {
+	var jason = window.localStorage.setItem('data', data);
+	JSON.stringify(jason);
+	console.log(jason);
+	})
+	.then(() => companies.removeChild(companies.firstChild))
 	.catch(error => console.error(error));
+	}
+	
+	
 }, false);
+ 
 
 document.addEventListener('DOMContentLoaded', () => {
-  event.preventDefault();
+	event.preventDefault();
 });
